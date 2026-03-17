a12ba68354a60426588167ddb4e5195403457778
Sarath <mcsarath2496@gmail.com>
2026-03-17 02:44:37 +0000

    Ec2 instance creation

--- main.tf
+++ main.tf
@@ -43,3 +43,15 @@ resource "aws_security_group" "sg" {
     Name = "${var.project_name}-${var.project_environment}-sg"
   }
 }
+
+resource "aws_instance" "webserver" {
+
+  ami                    = var.instance_ami
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.auth_key.key_name
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  user_data              = file("setup.sh")
+  tags = {
+    "Name" = "${var.project_name}-${var.project_environment}-webserver"
+  }
+}
